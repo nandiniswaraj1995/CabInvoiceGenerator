13ea7ae3aa04d18abd57c87b0471f9af2af98feb
unknown <nandiniswaraj13@gmail.com>
2021-04-06 11:23:04 +0530

    [Nandini swaraj]Added Given distance and time and calculate total fare

---
+++ CabInvoiceGenerator/CabInvoiceGeneratorNUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using CAbInvoiceGenarator;
+
+namespace CabInvoiceGeneratorNUnitTestProject1
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
